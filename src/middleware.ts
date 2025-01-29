e16694f169d41c8e023fb7cc3e87793d6621ce35
Mikhail Mikhailov <ashimka2010@yandex.ru>
2025-01-29 23:28:49 +0500

    fix: middleware auth page

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,15 @@ import { NextRequest, NextResponse } from "next/server";
 export async function middleware(req: NextRequest) {
   const token = req.cookies.get("RF")?.value;
 
+  const isAuthPage = req.url.includes("/auth");
+
+  if (isAuthPage) {
+    if (token) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+    return NextResponse.next();
+  }
+
   if (token === undefined) {
     return NextResponse.redirect(new URL("/auth", req.url));
   }
@@ -11,5 +20,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/profile/:path*"],
+  matcher: ["/profile/:path*", "/auth"],
 };
